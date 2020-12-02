03be00ce9f6a2346c47e1ba729e7c17a8829b03b
Rebecca Skinner <rebecca@rebeccaskinner.net>
2020-12-02 00:13:35 -0600

    2020 day 1

---
+++ 2020/day1/Day1.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE TupleSections #-}
+module Main where
+import qualified Data.List as List
+import System.Environment (getArgs)
+import Text.Printf (printf)
+import qualified Data.IntMap.Strict as Map
+import qualified Data.IntSet as Set
+
+pairPermutations :: [Int] -> [(Int,Int)]
+pairPermutations [] = []
+pairPermutations (x:xs) = map (x,) xs <> pairPermutations xs
+
+threeSum :: Int -> [Int] -> Maybe (Int,Int,Int)
+threeSum goal nums =
+  let
+    s = Set.fromList nums
+    pairs = pairPermutations nums
+    hasCompliment a b = Set.member (goal - (a + b)) s
+    res = List.find (uncurry hasCompliment) pairs
+  in case res of
+    Just (a,b) -> Just (a,b,goal - (a + b))
+    Nothing -> Nothing
+
+twoSum :: Int -> [Int] -> Maybe (Int,Int)
+twoSum goal nums =
+  List.find (\(a,b) -> a + b == goal) (pairPermutations nums)
+
+part1 :: FilePath -> IO ()
+part1 path = do
+  nums <- (map read . lines) <$> readFile path
+  case twoSum 2020 nums of
+    Nothing -> putStrLn "no pair found"
+    Just (a,b) -> putStrLn $ printf "%d * %d = %d" a b (a*b)
+
+part2 :: FilePath -> IO ()
+part2 path = do
+  nums <- (map read . lines) <$> readFile path
+  case threeSum 2020 nums of
+    Nothing -> putStrLn "no pair found"
+    Just (a,b,c) -> putStrLn $ printf "%d * %d * %d = %d" a b c (a*b*c)
+
+main :: IO ()
+main = do
+  (part:input:_) <- getArgs
+  case part of
+    "part1" -> part1 input
+    "part2" -> part2 input
+    _ -> error "unknown command"
