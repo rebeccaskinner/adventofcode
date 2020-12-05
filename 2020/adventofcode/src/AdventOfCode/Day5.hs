d84650a5cc7860c3dd6141c3788ae565d70b0212
Rebecca Skinner <rebecca@rebeccaskinner.net>
2020-12-05 14:14:36 -0600

    directly convert to a seat id

--- 2020/adventofcode/src/AdventOfCode/Day5.hs
+++ 2020/adventofcode/src/AdventOfCode/Day5.hs
@@ -1,49 +1,24 @@
-module AdventOfCode.Day5 where
+module AdventOfCode.Day5 (part1, part2, day5) where
 import           AdventOfCode.Types
 import           Control.Monad
 import           Control.Monad.ST
 import qualified Data.List          as List
-import           Data.Ord
+import Data.Bits
 import           GHC.Arr
 import           Text.Printf
 
-binSearch :: (a -> Ordering) -> Int -> Int -> [a] -> Int
-binSearch cmp l u [] = u
-binSearch cmp l u (x:xs) =
-  let
-    center = ((l + u) `div` 2)
-  in case cmp x of
-    GT -> binSearch cmp center u xs
-    LT -> binSearch cmp l center xs
-    _  -> error "binSearch got an unexpected comparison"
-
-seatOrdering :: Char -> Ordering
-seatOrdering c =
-  case c of
-    'F' -> LT
-    'L' -> LT
-    'B' -> GT
-    'R' -> GT
-    _   -> error "invalid input"
-
-findSeatPart1 :: String -> (Int,Int)
-findSeatPart1 s =
-  let (row,col) = splitAt 7 s
-  in (binSearch seatOrdering 0 127 row, binSearch seatOrdering 0 7 col)
-
-seatId :: Int -> Int -> Int
-seatId row col = (row * 8) + col
-
-part1 :: Puzzle IO ()
-part1 = withStringInput $ \i -> do
-  let seats = lines i
-  ids <- forM seats $ \seat -> do
-          let (row,col) = findSeatPart1 seat
-              i = seatId row col
-          putStrLn $ printf "%s: row %d, column %d, seat ID: %d" seat row col i
-          pure i
-  let maxid = maximum ids
-  putStrLn $ printf "maximum seat id: %d" maxid
+strToSeatId :: String -> Int
+strToSeatId =
+  List.foldl' (\seatId n -> shiftL seatId 1 .|. seatToBit n) 0
+  where
+    seatToBit :: Char -> Int
+    seatToBit c =
+      case c of
+        'F' -> 0
+        'L' -> 0
+        'B' -> 1
+        'R' -> 1
+        _ -> error "invalid input"
 
 maxSeatId = (127 * 8 + 7)
 
@@ -69,11 +44,17 @@ findEmptySeat arr =
 part2 :: Puzzle IO ()
 part2 = withStringInput $ \i ->
   let
-    seats = seatSet $ map (uncurry seatId . findSeatPart1) (lines i)
+    seats = seatSet $ map strToSeatId (lines i)
   in
     case findEmptySeat seats of
        Nothing     -> putStrLn "no empty seat available"
        Just seatId -> putStrLn $ printf "empty seat id: %d" seatId
+
+part1 :: Puzzle IO ()
+part1 = withStringInput $ \i -> do
+  let seats = map strToSeatId (lines i)
+      maxid = maximum seats
+  putStrLn $ printf "maximum seat id: %d" maxid
 
 day5 :: PuzzleDay IO
 day5 = PuzzleDay part1 part2
